79bbc707a41e158b53b5ce4be11d4eb9efced9f6
WangCong <a1e2w3@126.com>
2020-04-24 21:58:15 +0800

    update

--- go.mod
+++ go.mod
@@ -4,11 +4,9 @@ go 1.12
 
 require (
 	github.com/Shopify/sarama v1.26.1
-	github.com/bsm/sarama-cluster v2.1.15+incompatible
 	github.com/elastic/go-elasticsearch/v6 v6.8.5
 	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b
-	github.com/onsi/ginkgo v1.12.0 // indirect
-	github.com/onsi/gomega v1.9.0 // indirect
 	github.com/rcrowley/go-metrics v0.0.0-20200313005456-10cdbea86bc0
+	golang.org/x/net v0.0.0-20200202094626-16171245cfb2
 	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c
 )
--- server/consumer.go
+++ server/consumer.go
@@ -2,27 +2,29 @@ package server
 
 import (
 	"fmt"
+	"golang.org/x/net/context"
 	"sync"
 	"time"
 
 	"github.com/ParticleMedia/nonlocal-indexer/common"
 	"github.com/Shopify/sarama"
-	cluster "github.com/bsm/sarama-cluster"
 	"github.com/golang/glog"
 	"github.com/rcrowley/go-metrics"
 )
 
 type KafkaConsumer struct {
 	name         string
+	conf         *common.KafkaConfig
 	commitOffset bool
 	worker       uint32
-	consumer     *cluster.Consumer
+	client       sarama.ConsumerGroup
 	swg          sync.WaitGroup
+	cancel       context.CancelFunc
 }
 
 func NewKafkaConsumer(name string, conf *common.KafkaConfig) (*KafkaConsumer, error) {
-	config := cluster.NewConfig()
-	config.Group.Return.Notifications = true
+	config := sarama.NewConfig()
+	config.Consumer.Return.Errors = true
 	config.Consumer.Offsets.Initial = sarama.OffsetNewest //初始从最新的offset开始
 	var commitOffset bool = (conf.CommitInterval > 0)
 	config.Consumer.Offsets.AutoCommit.Enable = commitOffset
@@ -32,10 +34,9 @@ func NewKafkaConsumer(name string, conf *common.KafkaConfig) (*KafkaConsumer, er
 	} else {
 		// 不提交commit
 		glog.Infof("do not commit kafka")
-		config.Consumer.Offsets.AutoCommit.Interval = time.Second
 	}
 
-	c, err := cluster.NewConsumer(conf.Addrs, conf.GroupId, conf.Topics, config)
+	c, err := sarama.NewConsumerGroup(conf.Addrs, conf.GroupId, config)
 	if err != nil {
 		glog.Errorf("Failed open consumer: %v", err)
 		return nil, err
@@ -43,18 +44,22 @@ func NewKafkaConsumer(name string, conf *common.KafkaConfig) (*KafkaConsumer, er
 
 	return &KafkaConsumer{
 		name:         name,
+		conf:         conf,
 		commitOffset: commitOffset,
 		worker:       conf.Consumer,
-		consumer:     c,
+		client:       c,
 		swg:          sync.WaitGroup{},
 	}, nil
 }
 
 func (c *KafkaConsumer) Close() error {
-	if c.consumer != nil {
-		err := c.consumer.Close()
+	if c.cancel != nil {
+		c.cancel()
+	}
+	if c.client != nil {
+		err := c.client.Close()
 		c.swg.Wait()
-		c.consumer = nil
+		c.client = nil
 		glog.Infof("kafka consumer exited.")
 		return err
 	}
@@ -64,52 +69,112 @@ func (c *KafkaConsumer) Close() error {
 type MessageHandler func([]byte) error
 
 // consumer 消费者
-func (c *KafkaConsumer) Consume(handler MessageHandler) {
-	// workers
-	for i := uint32(0); i < c.worker; i++ {
-		c.swg.Add(1)
-		go func() {
-			defer c.swg.Done()
-			for {
-				select {
-				case msg, ok := <-c.consumer.Messages():
-					if ok {
-						c.handleMessage(msg, handler)
-						if c.commitOffset {
-							c.consumer.MarkOffset(msg, "")
-						}
-					} else {
-						return
-					}
-				case err := <-c.consumer.Errors():
-					if err != nil {
-						glog.Errorln(err)
-					}
-				case n := <-c.consumer.Notifications():
-					if n != nil {
-						glog.Infof("notification: %+v", n)
-					}
-				}
-			}
-		}()
+func (c *KafkaConsumer) Consume(f MessageHandler) {
+	handler := &KafkaHandler{
+		name: c.name,
+		commitOffset: c.commitOffset,
+		process: f,
+		worker: c.worker,
+		ready: make(chan bool),
+		swg: sync.WaitGroup{},
 	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	c.cancel = cancel
+	c.swg.Add(1)
+	go func() {
+		defer c.swg.Done()
+		for {
+			// `Consume` should be called inside an infinite loop, when a
+			// server-side rebalance happens, the consumer session will need to be
+			// recreated to get the new claims
+			if err := c.client.Consume(ctx, c.conf.Topics, handler); err != nil {
+				glog.Fatalf("Consume Error: %+v", err)
+			}
+			// check if context was cancelled, signaling that the consumer should stop
+			if ctx.Err() != nil {
+				glog.Infof("consumer stopped")
+				return
+			}
+			handler.ready = make(chan bool)
+		}
+	}()
+
+	// log client errors
+	go func() {
+		defer c.swg.Done()
+		for err := range c.client.Errors() {
+			glog.Warningf("Error from client: %+v", err)
+		}
+	} ()
+
+	// wait until handler.Setup has been called
+	<-handler.ready
 }
 
 func (c *KafkaConsumer) Join() {
 	c.swg.Wait()
 }
 
-func (c *KafkaConsumer) handleMessage(msg *sarama.ConsumerMessage, handler MessageHandler) {
+
+// KafkaHandler represents a Sarama consumer group handler
+type KafkaHandler struct {
+	name string
+	commitOffset bool
+	process MessageHandler
+	worker uint32
+	ready chan bool
+	swg sync.WaitGroup
+}
+
+// Setup is run at the beginning of a new session, before ConsumeClaim
+func (h *KafkaHandler) Setup(sarama.ConsumerGroupSession) error {
+	// Mark the consumer as ready
+	close(h.ready)
+	return nil
+}
+
+// Cleanup is run at the end of a session, once all ConsumeClaim goroutines have exited
+func (h *KafkaHandler) Cleanup(sarama.ConsumerGroupSession) error {
+	return nil
+}
+
+// ConsumeClaim must start a consumer loop of ConsumerGroupClaim's Messages().
+func (h *KafkaHandler) ConsumeClaim(session sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim) error {
+
+	// NOTE:
+	// Do not move the code below to a goroutine.
+	// The `ConsumeClaim` itself is called within a goroutine, see:
+	// https://github.com/Shopify/sarama/blob/master/consumer_group.go#L27-L29
+
+	// workers
+	h.swg.Add(int(h.worker))
+	for i := uint32(0); i < h.worker; i++ {
+		go func() {
+			defer h.swg.Done()
+			for msg := range claim.Messages() {
+				h.handleMessage(msg)
+				if h.commitOffset {
+					session.MarkMessage(msg, "")
+				}
+			}
+		}()
+	}
+	h.swg.Wait()
+	return nil
+}
+
+func (h *KafkaHandler) handleMessage(msg *sarama.ConsumerMessage) {
 	defer func(start time.Time) {
-		metrics.GetOrRegisterTimer(fmt.Sprintf("consume.%s.latency", c.name), nil).Update(time.Since(start))
+		metrics.GetOrRegisterTimer(fmt.Sprintf("consume.%s.latency", h.name), nil).Update(time.Since(start))
 	}(time.Now())
 
-	metrics.GetOrRegisterMeter(fmt.Sprintf("consume.%s.qps", c.name), nil).Mark(1)
-	err := handler(msg.Value)
+	metrics.GetOrRegisterMeter(fmt.Sprintf("consume.%s.qps", h.name), nil).Mark(1)
+	err := h.process(msg.Value)
 	if err == nil {
-		metrics.GetOrRegisterHistogram(fmt.Sprintf("consume.%s.error", c.name), nil, metrics.NewExpDecaySample(1024, 0.015)).Update(0)
+		metrics.GetOrRegisterHistogram(fmt.Sprintf("consume.%s.error", h.name), nil, metrics.NewExpDecaySample(1024, 0.015)).Update(0)
 	} else {
 		glog.Warningf("handle message error: %+v", err)
-		metrics.GetOrRegisterHistogram(fmt.Sprintf("consume.%s.error", c.name), nil, metrics.NewExpDecaySample(1024, 0.015)).Update(100)
+		metrics.GetOrRegisterHistogram(fmt.Sprintf("consume.%s.error", h.name), nil, metrics.NewExpDecaySample(1024, 0.015)).Update(100)
 	}
 }
